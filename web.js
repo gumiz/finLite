f3cd276010bce51fe04ca9f0d1368f23e8ec32d8
gumiz <micgrn@gmail.com>
2015-04-28 17:56:38 +0200

    body-parser

--- web.js
+++ web.js
@@ -2,10 +2,13 @@ var bodyParser = require("body-parser");
 var express = require('express');
 var app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({
+//  extended: true
+//}));
+//app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.set('port', (process.env.PORT || 9000));
 app.use(express.static(__dirname + '/app'));
