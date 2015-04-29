3a13bf88f902313b711798d8c745351dc99d4511
gumiz <micgrn@gmail.com>
2015-04-29 17:52:37 +0200

    routing

--- web.js
+++ web.js
@@ -2,6 +2,15 @@ var bodyParser = require("body-parser");
 var express = require('express');
 var app = express();
 
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://finlite:1@ds055689.mongolab.com:55689/finlite", {native_parser:true});
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
+require('routes/accountsRoute')(app);
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -10,16 +19,9 @@ app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 9000));
 app.use(express.static(__dirname + '/dist'));
 
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
-});
-
-var mongo = require('mongoskin');
-var db = mongo.db("mongodb://finlite:1@ds055689.mongolab.com:55689/finlite", {native_parser:true});
-
-app.use(function(req,res,next){
-  req.db = db;
-  next();
 });
 
 //require('./routes/test')(app);
@@ -27,25 +29,25 @@ app.use(function(req,res,next){
 //	res.send('Test !');
 //});
 
-app.get('/accounts', function(req, res) {
-    var db = req.db;
-    db.collection('accounts').find().toArray(function (err, items) {
-        res.json(items);
-    });
-});
-
-app.post('/addAccount', function(req, res) {
-  var db = req.db;
-  db.collection('accounts').insert(req.body, function(err, result){
-    res.send(
-      (err === null) ? { msg: '' } : { msg: err }
-    );
-  });
-});
-
-app.post('/deleteAccount', function(req, res) {
-  var db = req.db;
-  db.collection('accounts').removeById(req.body.id, function(err, result) {
-    res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
-  });
-});
+//app.get('/accounts', function(req, res) {
+//    var db = req.db;
+//    db.collection('accounts').find().toArray(function (err, items) {
+//        res.json(items);
+//    });
+//});
+//
+//app.post('/addAccount', function(req, res) {
+//  var db = req.db;
+//  db.collection('accounts').insert(req.body, function(err, result){
+//    res.send(
+//      (err === null) ? { msg: '' } : { msg: err }
+//    );
+//  });
+//});
+//
+//app.post('/deleteAccount', function(req, res) {
+//  var db = req.db;
+//  db.collection('accounts').removeById(req.body.id, function(err, result) {
+//    res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
+//  });
+//});
