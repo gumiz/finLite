8575a446cae3e7b69d1390fde884b88703b7eec4
gumiz <micgrn@gmail.com>
2015-04-30 23:07:10 +0200

    fixed auto number when empty documents

--- app/scripts/controllers/documents.js
+++ app/scripts/controllers/documents.js
@@ -2,31 +2,33 @@
 
 angular.module('finLiteApp').controller('DocumentsCtrl', ['$scope', 'repositoryService', 'dialogService', 'notify', 'datepickerService', 'dateUtils', function ($scope, repositoryService, dialogService, notify, datepickerService, dateUtils) {
   $scope.newDocument = {};
-  var gotDocuments = function(documents){
+  var gotDocuments = function (documents) {
     $scope.documents = documents;
     notify.info('odświeżono dane');
   };
-  var refresh = function() {
+  var refresh = function () {
     repositoryService.getDocuments(gotDocuments);
   };
   $scope.refresh = refresh;
 
-  $scope.addDocument = function() {
+  $scope.addDocument = function () {
     setAutoNumber();
     fixPrice();
     $scope.newDocument.date = dateUtils.dateToString($scope.newDocument.date);
     repositoryService.addDocument($scope.newDocument, refresh);
   };
 
-  var deleteAccount = function(id){
-    return function() {return repositoryService.deleteDocument(id, refresh)};
+  var deleteAccount = function (id) {
+    return function () {
+      return repositoryService.deleteDocument(id, refresh)
+    };
   };
 
-  $scope.deleteDocument = function(id) {
+  $scope.deleteDocument = function (id) {
     dialogService.confirmation('Czy na pewno chcesz usunąć dokument?', deleteAccount(id));
   };
 
-  var gotAccounts = function(items){
+  var gotAccounts = function (items) {
     $scope.accounts = items;
   };
   repositoryService.getAccounts(gotAccounts);
@@ -34,15 +36,22 @@ angular.module('finLiteApp').controller('DocumentsCtrl', ['$scope', 'repositoryS
   refresh();
 
   function setAutoNumber() {
-    var maxNumber = _.max($scope.documents, function(item){
-      var result =  isNaN(item.autoNumber) ? 0 : item.autoNumber;
-      return result;
-    });
-    $scope.newDocument.autoNumber = Number(maxNumber.autoNumber) + 1;
+    var result = 1;
+    if ($scope.documents.length != 0) {
+      var maxNumber = _.max($scope.documents, function (item) {
+        var result = isNaN(item.autoNumber) ? 0 : item.autoNumber;
+        return result;
+      });
+      result = Number(maxNumber.autoNumber) + 1;
+    }
+    $scope.newDocument.autoNumber = result;
   }
 
   function fixPrice() {
-    $scope.newDocument.price = $scope.newDocument.price.replace(',', '.');
+    debugger;
+    if ($scope.newDocument.price) {
+      $scope.newDocument.price = $scope.newDocument.price.replace(',', '.');
+    }
   }
 
 }]);
